b12eb188fcb23deff7ff0c553c1525378b24f7dc
Axel Huebl <axel.huebl@plasma.ninja>
2019-05-30 00:28:54 +0200

    mpiexec: work-arounds from PHDF5

--- recipe/build.sh
+++ recipe/build.sh
@@ -40,6 +40,18 @@ if [[ ${mpi} == "nompi" ]]; then
 else
     export USE_MPI=ON
 fi
+#   see https://github.com/conda-forge/hdf5-feedstock/blob/master/recipe/mpiexec.sh
+if [[ "$mpi" == "mpich" ]]; then
+    export HYDRA_LAUNCHER=fork
+    #export HYDRA_LAUNCHER=ssh
+fi
+if [[ "$mpi" == "openmpi" ]]; then
+    export OMPI_MCA_btl=self,tcp
+    export OMPI_MCA_plm=isolated
+    #export OMPI_MCA_plm=ssh
+    export OMPI_MCA_rmaps_base_oversubscribe=yes
+    export OMPI_MCA_btl_vader_single_copy_mechanism=none
+fi
 
 cmake \
     -DCMAKE_BUILD_TYPE=Release  \
