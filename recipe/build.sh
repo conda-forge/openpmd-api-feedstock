571f81decd50cc9907bd41acc0227eb682674bb8
Axel Huebl <axel.huebl@plasma.ninja>
2025-09-22 00:29:42 -0700

    libz: dylib

--- recipe/build.sh
+++ recipe/build.sh
@@ -100,11 +100,11 @@ if [[ ${target_platform} =~ osx.* ]]; then
     sed -i "s@${CONDA_BUILD_SYSROOT}/usr/lib/libdl.tbd@-ldl.tbd@g"             $PREFIX/lib/cmake/openPMD/openPMDTargets.cmake
     sed -i "s@${CONDA_BUILD_SYSROOT}/usr/lib/libm.tbd@-lm.tbd@g"               $PREFIX/lib/cmake/openPMD/openPMDTargets.cmake
     sed -i "s@${CONDA_BUILD_SYSROOT}/usr/lib/libpthread.tbd@-lpthread@g"       $PREFIX/lib/cmake/openPMD/openPMDTargets.cmake
-    sed -i "s@${CONDA_BUILD_SYSROOT}/usr/lib/libz.tbd@\$PREFIX/lib/libz.tbd@g" $PREFIX/lib/cmake/openPMD/openPMDTargets.cmake
+    sed -i "s@${CONDA_BUILD_SYSROOT}/usr/lib/libz.tbd@\$PREFIX/lib/libz.dylib@g" $PREFIX/lib/cmake/openPMD/openPMDTargets.cmake
     sed -i "s@${CONDA_BUILD_SYSROOT}/usr/lib/libdl.tbd@-ldl@g"                 $PREFIX/lib/pkgconfig/openPMD.pc
     sed -i "s@${CONDA_BUILD_SYSROOT}/usr/lib/libm.tbd@-lm@g"                   $PREFIX/lib/pkgconfig/openPMD.pc
     sed -i "s@${CONDA_BUILD_SYSROOT}/usr/lib/libpthread.tbd@-lpthread@g"       $PREFIX/lib/pkgconfig/openPMD.pc
-    sed -i "s@${CONDA_BUILD_SYSROOT}/usr/lib/libz.tbd@\$PREFIX/lib/libz.tbd@g" $PREFIX/lib/pkgconfig/openPMD.pc
+    sed -i "s@${CONDA_BUILD_SYSROOT}/usr/lib/libz.tbd@\$PREFIX/lib/libz.dylib@g" $PREFIX/lib/pkgconfig/openPMD.pc
     #   3) grep for CONDA_BUILD_SYSROOT in installed files again -- fail if still present
     echo "Hard-coded sysroot paths:"
     ! grep -iIR "${CONDA_BUILD_SYSROOT}" ${PREFIX}
